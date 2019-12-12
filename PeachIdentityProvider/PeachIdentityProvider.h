2b3c383bf62e61719e5b7d44a7ec9d59b1338abd
Rayan Arnaout <arnaout@ebu.ch>
2019-12-12 16:19:46 +0100

    Initial Commit

---
+++ PeachIdentityProvider/PeachIdentityProvider.h
@@ -0,0 +1,19 @@
+//
+//  PeachIdentityProvider.h
+//  PeachIdentityProvider
+//
+//  Created by Rayan Arnaout on 12.12.19.
+//  Copyright © 2019 European Broadcasting Union. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PeachIdentityProvider.
+FOUNDATION_EXPORT double PeachIdentityProviderVersionNumber;
+
+//! Project version string for PeachIdentityProvider.
+FOUNDATION_EXPORT const unsigned char PeachIdentityProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PeachIdentityProvider/PublicHeader.h>
+
+
---
+++ PeachIdentityProviderTests/PeachIdentityProviderTests.m
@@ -0,0 +1,37 @@
+//
+//  PeachIdentityProviderTests.m
+//  PeachIdentityProviderTests
+//
+//  Created by Rayan Arnaout on 12.12.19.
+//  Copyright © 2019 European Broadcasting Union. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PeachIdentityProviderTests : XCTestCase
+
+@end
+
+@implementation PeachIdentityProviderTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
