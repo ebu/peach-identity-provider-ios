4aa4cabcc10d22c54c8b4bceda04bf99330cafe3
Eoin O’Connell <eoin@8bytes.ie>
2023-01-30 11:19:58 +0000

    Add dependencies to SPM target

--- Package.swift
+++ Package.swift
@@ -5,6 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "PeachIdentityProvider",
+    platforms: [.iOS(.v11),
+                .tvOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -26,7 +28,13 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "PeachIdentityProvider"
+            name: "PeachIdentityProvider",
+            dependencies: [
+                "FXReachability",
+                "libextobjc",
+                "MAKVONotificationCenter",
+                "UICKeyChainStore",
+            ]
         )
     ]
 )
